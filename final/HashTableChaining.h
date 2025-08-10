03087261ce1d93782219fc7743540bc332f3be52
EmilioG0103 <egonzalez45648@ucq.edu.mx>
2025-08-10 17:08:56 -0600

    Update HashTableChaining.h

--- final/HashTableChaining.h
+++ final/HashTableChaining.h
@@ -1,90 +1,65 @@
-#pragma once
+#ifndef HASHTABLECHAINING_H
+#define HASHTABLECHAINING_H
 
+#include <forward_list>
 #include <iostream>
-#include <forward_list> 
+#include <stdexcept>
 #include <string>
 using namespace std;
 
 template <typename T>
 class HashTableChaining
 {
-protected:
-	forward_list<T>* data; 
-	unsigned int arraySize;
-	
+private:
+    forward_list<T>* data;
+    unsigned int arraySize;
+
 public:
-	HashTableChaining(unsigned int size)
-	{
-		data = new forward_list<T>[size];
-		arraySize = size;
-	}
+    HashTableChaining(unsigned int size) {
+        data = new forward_list<T>[size];
+        arraySize = size;
+    }
 
-	int HashFunction(T key)
-	{
-		return key % arraySize;
-	}
+    int HashFunction(T key) {
+        return key % arraySize;
+    }
 
-	virtual void Add(T element)
-	{
-		int index = HashFunction(element);
-		data[index].push_front(element);
-	}
+    void Add(T element) {
+        int index = HashFunction(element);
+        data[index].push_front(element);
+    }
 
-	void Remove(T element)
-	{
-		int index = HashFunction(element);
-		forward_list<T>& listAtIndex = data[index];
-		for (auto i : listAtIndex)
-		{
-			if (i == element)
-			{
-				listAtIndex.remove(i);
-				return;
-			}
-		}
-		throw runtime_error("no element " + to_string(element) + " in this hash table.");
-	}
+    void Remove(T element) {
+        int index = HashFunction(element);
+        forward_list<T>& listAtIndex = data[index];
+        for (auto i : listAtIndex) {
+            if (i == element) {
+                listAtIndex.remove(i);
+                return;
+            }
+        }
+        throw runtime_error("No element " + to_string(element) + " in this hash table.");
+    }
 
-	bool Contains(T element)
-	{
-		int index = HashFunction(element);
-		forward_list<T>& listAtIndex = data[index];
-		for (auto i : listAtIndex)
-		{
-			if (i == element)
-			{
-				return true;
-			}
-		}
-		return false;
-	}
+    bool Contains(T element) {
+        int index = HashFunction(element);
+        forward_list<T>& listAtIndex = data[index];
+        for (auto i : listAtIndex) {
+            if (i == element) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	void Print()
-	{
-		for (int i = 0; i < arraySize; i++)
-		{
-			cout << "lista del índice: " << std::to_string(i) << ": ";
-			for (auto j : data[i])
-				cout << j << ", ";
-			cout << endl;
-		}
-	}
+    void Print() {
+        for (int i = 0; i < arraySize; i++) {
+            cout << "Lista del índice " << i << ": ";
+            for (auto j : data[i])
+                cout << j << ", ";
+            cout << endl;
+        }
+    }
 };
 
-// Clase HashSet 
-template <typename T>
-class HashSet : public HashTableChaining<T>
-{
-public:
-	HashSet(unsigned int size) : HashTableChaining<T>(size) {}
-
-	void Add(T element) override
-	{
-		if (!this->Contains(element))
-		{
-			HashTableChaining<T>::Add(element);
-		}
-	}
-};
-
-void HashTableChainingExample();
+#endif
